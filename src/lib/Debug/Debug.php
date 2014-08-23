3973e0d6cf06d18015876554eac7df75e3b2c332
NK-WEB-Git <nkuinjeustephane@gmail.com>
2014-08-24 01:15:56 +0200

    Debug #improve code file

--- src/lib/Debug/Debug.php
+++ src/lib/Debug/Debug.php
@@ -3,6 +3,21 @@
 namespace Debug;
 
 Class Debug  {
+
+	public function getDataServer() {
+
+		return var_dump($_SERVER)
+	}
+
+	public function getDataConst() {
+
+		return $const;
+	}
+
+	public function getDataSession() {
+
+		return var_dump($_SESSION);
+	}
 	
 }
 
